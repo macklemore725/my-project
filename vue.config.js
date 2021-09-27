a5167542e0c9a0c119c957be75fb0009827abda4
macklemore725 <78071295+macklemore725@users.noreply.github.com>
2021-09-27 14:50:00 +0800

    Try to Fix Fix this problem =>  <noscript>       <strong>We're sorry but client doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>     </noscript>

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
+  runtimeCompiler: true,
   publicPath:
     process.env.NODE_ENV === "production" ? "/my-project/" : "/csb-bmrt5"
 };

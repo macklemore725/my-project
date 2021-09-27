6b24ac98e51aef5bf1ddd1f376d4aef5b6d9c494
macklemore725 <78071295+macklemore725@users.noreply.github.com>
2021-09-27 12:18:26 +0800

    Add Pubic Path add public Path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/my-project/" : "/"
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/my-project/" : "/csb-bmrt5"
 };
